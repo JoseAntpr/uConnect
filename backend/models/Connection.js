b2cbe69ee911c202cd5c6935c438870ad3c52fdb
José Antonio Palacios Ramírez <joseantpalaciosramirez@gmail.com>
2018-10-07 16:12:00 +0200

    ADD: Connection model

---
+++ backend/models/Connection.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+let valid_connections = {
+    values: ['FAMILY', 'FRIENDS', 'UNFAMILIAR'],
+    message: '{VALUE} is not valid connection'
+}
+
+let Schema = mongoose.Schema;
+
+let connectionSchema = new Schema({
+    connection: {
+        type: String,
+        default: 'UNFAMILIAR',
+        enum: valid_connections,
+        required: [true, 'Connection type is required']
+    },
+    userOne: { type: Schema.Types.ObjectId, ref: 'User', required: true},
+    userTwo: { type: Schema.Types.ObjectId, ref: 'User', required: true }
+});
+
+module.exports = mongoose.model('Connection', connectionSchema);
+
