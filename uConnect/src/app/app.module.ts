46ca5ebcc3403f31a59364d413c8385667e45f3f
José Antonio Palacios Ramírez <joseantpalaciosramirez@gmail.com>
2018-10-07 18:21:53 +0200

    ADD: navbar & userList Components

--- uConnect/src/app/app.module.ts
+++ uConnect/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UserListComponent } from './components/user-list/user-list.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserListComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
