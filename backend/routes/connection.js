5dbb0f2da5a8b764b93f8d7fd98167fb3107e9f4
José Antonio Palacios Ramírez <joseantpalaciosramirez@gmail.com>
2018-10-08 21:05:53 +0200

    ADD: get user list not coneccted by one user

--- backend/routes/connection.js
+++ backend/routes/connection.js
@@ -1,82 +1,124 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-const Connection = require('../models/Connection');
+const Connection = require("../models/Connection");
+const User = require("../models/User");
 
-app.get('/connection/:user', (req, res) => {
-    let userId = req.params.user;
+app.get("/connected/:user", (req, res) => {
+  let userId = req.params.user;
 
-    Connection.find({$or:[{userOne: userId},{userTwo:userId}]})
+  Connection.find({ $or: [{ userOne: userId }, { userTwo: userId }] })
     .populate("userOne", "name")
     .populate("userTwo", "name")
     .exec((err, connections) => {
-        if ( err ) {
-            return res.status(400).json({
-                ok: false,
-                error: err
-            });
-        }
-
-        connections = connections.map( connection => {
-            let user;
-
-            if( String(connection.userOne._id) === userId ) {
-                user = connection.userTwo;
-            } 
-            
-            if ( String(connection.userTwo._id) === userId ) {
-                user = connection.userOne;
-            }
-            return {
-                connection: connection.connection,
-                user: user
-            }
-        }
-        )
-
-        res.status(200).json({
-            ok: true,
-            connections: connections
+      if (err) {
+        return res.status(400).json({
+          ok: false,
+          error: err
         });
+      }
+
+      connections = connections.map(connection => {
+        let user;
+
+        if (String(connection.userOne._id) === userId) {
+          user = connection.userTwo;
+        }
+
+        if (String(connection.userTwo._id) === userId) {
+          user = connection.userOne;
+        }
+        return {
+          connection: connection.connection,
+          user: user
+        };
+      });
+
+      res.status(200).json({
+        ok: true,
+        connections: connections
+      });
     });
 });
 
+app.get("/connection/:user", (req, res) => {
+  let userId = req.params.user;
 
-app.post('/connection', (req, res) => {
-    let body = req.body;
-    console.log(body)
-    console.log( req.body)
-    console.log(body.userOne == body.userTwo)
-    console.log(body.userOne, body.userTwo)
-    if( body.userOne == body.userTwo) {
-        return res.status(409).json({
-            ok: false,
-            error: 'Not allowed that UserOne and UserTwo are the same value'
+  Connection.find({ $or: [{ userOne: userId }, { userTwo: userId }] })
+    .populate("userOne", "name")
+    .populate("userTwo", "name")
+    .exec((err, connections) => {
+      if (err) {
+        return res.status(400).json({
+          ok: false,
+          error: err
         });
+      }
+
+      connections = connections.map(connection => {
+        let user;
+        let users;
+
+        if (String(connection.userOne._id) === userId) {
+          user = connection.userTwo;
+        }
+
+        if (String(connection.userTwo._id) === userId) {
+          user = connection.userOne;
+        }
+
+        User.find((err, usersDB) => {
+          users = usersDB.filter(u => {
+              return u.id !== user.id && u.id !== userId;
+          });
+
+          res.status(200).json({
+            ok: true,
+            connections: users.map( u => {
+                return {
+                    connection: connection.connection,
+                    user: u
+                }
+            })
+          });
+        });
+      });
+    });
+});
+
+app.post("/connection", (req, res) => {
+  let body = req.body;
+  console.log(body);
+  console.log(req.body);
+  console.log(body.userOne == body.userTwo);
+  console.log(body.userOne, body.userTwo);
+  if (body.userOne == body.userTwo) {
+    return res.status(409).json({
+      ok: false,
+      error: "Not allowed that UserOne and UserTwo are the same value"
+    });
+  }
+
+  let connection = new Connection({
+    connection: body.connection,
+    userOne: body.userOne,
+    userTwo: body.userTwo
+  });
+
+  connection.save((err, connectionDB) => {
+    if (err) {
+      return res.status(400).json({
+        ok: false,
+        error: err
+      });
     }
 
-    let connection =  new Connection({
-        connection: body.connection,
-        userOne: body.userOne,
-        userTwo: body.userTwo
+    res.status(200).json({
+      ok: true,
+      user: connectionDB
     });
-
-
-    connection.save(( err, connectionDB ) => {
-        if ( err ) {
-            return res.status(400).json({
-                ok: false,
-                error: err
-            });
-        }
-
-        res.status(200).json({
-            ok: true,
-            user: connectionDB
-        });
-    })
+  });
 });
-
 
 module.exports = app;
