85fefcd2d52515f9ae6852c203b9214a026ff94c
José Antonio Palacios Ramírez <joseantpalaciosramirez@gmail.com>
2018-10-11 18:24:58 +0200

    ADD: delete version key of User model

--- backend/models/User.js
+++ backend/models/User.js
@@ -4,6 +4,6 @@ let Schema = mongoose.Schema;
 
 let userSchema = new Schema({
     name: {type: String, required: [true, 'Name is required']}
-});
+}, { versionKey: false });
 
 module.exports = mongoose.model('User', userSchema);
