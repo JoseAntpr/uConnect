5f262026a51d618d7213261cbe364089fe383063
José Antonio Palacios Ramírez <joseantpalaciosramirez@gmail.com>
2018-10-12 14:45:53 +0200

    ADD: change connection routes callbak hell to async await

--- backend/routes/connection.js
+++ backend/routes/connection.js
@@ -5,103 +5,87 @@ const app = express();
 const Connection = require("../models/Connection");
 const User = require("../models/User");
 
-app.get("/connected/:user", (req, res) => {
+app.get("/connected/:user", async (req, res, next) => {
   let userId = req.params.user;
 
-  Connection.find({ $or: [{ userOne: userId }, { userTwo: userId }] })
-    .populate("userOne", "name")
-    .populate("userTwo", "name")
-    .exec((err, connections) => {
-      if (err) {
-        return res.status(400).json({
-          ok: false,
-          error: err
-        });
+  try {
+    connections = await Connection.find({
+      $or: [{ userOne: userId }, { userTwo: userId }]
+    })
+      .populate("userOne", "name")
+      .populate("userTwo", "name");
+
+    connections = connections.map(connection => {
+      let user = connection.userTwo;
+
+      if (String(connection.userTwo._id) === userId) {
+        user = connection.userOne;
       }
-
-      connections = connections.map(connection => {
-        let user;
-
-        if (String(connection.userOne._id) === userId) {
-          user = connection.userTwo;
-        }
-
-        if (String(connection.userTwo._id) === userId) {
-          user = connection.userOne;
-        }
-        return {
-          connection: connection.connection,
-          user: user
-        };
-      });
-
-      res.status(200).json({
-        ok: true,
-        connections: connections
-      });
+      return {
+        connection: connection.connection,
+        user: user
+      };
     });
+
+    res.status(200).json({
+      ok: true,
+      connections: connections
+    });
+  } catch (e) {
+    return next(e);
+  }
 });
 
-app.get("/connection/:user", (req, res) => {
+app.get("/connection/:user", async (req, res, next) => {
   let userId = req.params.user;
-  Connection.find({ $or: [{ userOne: userId }, { userTwo: userId }] })
-    .populate("userOne", "name")
-    .populate("userTwo", "name")
-    .exec((err, connections) => {
-      if (err) {
-        return res.status(400).json({
-          ok: false,
-          error: err
-        });
+
+  try {
+    let connections = await Connection.find({
+      $or: [{ userOne: userId }, { userTwo: userId }]
+    })
+      .populate("userOne", "name")
+      .populate("userTwo", "name");
+
+    connections = connections.map(connection => {
+      let user = connection.userTwo;
+
+      if (String(connection.userTwo._id) === userId) {
+        user = connection.userOne;
       }
 
-      connections = connections.map(connection => {
-        let user;
-
-        if (String(connection.userOne._id) === userId) {
-          user = connection.userTwo;
-        }
-
-        if (String(connection.userTwo._id) === userId) {
-          user = connection.userOne;
-        }
-
-        return user;
-      });
-
-
-      User.find({ _id: { $ne: userId } }, (err, usersDB) => {
-
-        let users;
-
-        if (connections.length === 0) {
-          users = usersDB
-        } else {
-
-          users = connections.map((user, index) => {
-            let res = usersDB.filter(u => {
-              return u._id.toString() !== user._id.toString();
-            });
-            usersDB = res;
-            return res;
-          })[connections.length-1];
-        }
-
-        return res.status(200).json({
-          ok: true,
-          users: users
-        });
-      });
+      return user;
     });
+
+    let users = await User.find({ _id: { $ne: userId } });
+
+    if (connections.length !== 0) {
+      users = connections.map(user => {
+        let res = users.filter(u => {
+          return u._id.toString() !== user._id.toString();
+        });
+        users = res;
+        return res;
+      })[connections.length - 1];
+    }
+
+    return res.status(200).json({
+      ok: true,
+      users: users
+    });
+  } catch (e) {
+    return next(e);
+  }
 });
 
-app.post("/connection", (req, res) => {
+app.post("/connection", async (req, res, next) => {
   let body = req.body;
+
   if (body.userOne == body.userTwo) {
-    return res.status(409).json({
-      ok: false,
-      error: "Not allowed that UserOne and UserTwo are the same value"
-    });
+    let error = new Error(
+      "Not allowed that UserOne and UserTwo are the same value"
+    );
+    error.status = 409;
+    return next(error);
   }
 
   let connection = new Connection({
@@ -110,19 +94,16 @@ app.post("/connection", (req, res) => {
     userTwo: body.userTwo
   });
 
-  connection.save((err, connectionDB) => {
-    if (err) {
-      return res.status(400).json({
-        ok: false,
-        error: err
-      });
-    }
+  try {
+    connectionDB = await connection.save();
 
     res.status(200).json({
       ok: true,
       users: connectionDB
     });
-  });
+  } catch (e) {
+    return next(e);
+  }
 });
 
 module.exports = app;
